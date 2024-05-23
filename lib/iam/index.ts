dfe7a2ca4ff3acbb7823f885e8cd5f650c8be321
Chris Dobson <chrd@trustpilot.com>
2024-05-23 22:08:50 +0100

    replace full access policies

--- lib/iam/index.ts
+++ lib/iam/index.ts
@@ -12,13 +12,58 @@ export const create = ({ scope, namespace }: { scope: Construct; namespace: stri
       new iam.ServicePrincipal("apigateway.amazonaws.com"),
     ),
     managedPolicies: [
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess"),
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEventBridgeFullAccess"),
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSQSFullAccess"),
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AWSStepFunctionsFullAccess"),
       iam.ManagedPolicy.fromAwsManagedPolicyName("AWSXrayFullAccess"),
       iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess"),
       iam.ManagedPolicy.fromAwsManagedPolicyName("SecretsManagerReadWrite"),
     ],
+    inlinePolicies: {
+      [`${namespace}-dynamo-policy`]: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["dynamodb:*"],
+            resources: [`arn:aws:dynamodb:*:*:table/${namespace}-*/stream/*`, `arn:aws:dynamodb:*:*:table/${namespace}-*/index/*`, `arn:aws:dynamodb:*:*:table/${namespace}-*`],
+          }),
+        ],
+      }),
+      [`${namespace}-eventbridge-policy`]: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["events:*"],
+            resources: [`arn:aws:events:*:*:event-bus/${namespace}-*`, `arn:aws:events:*:*:rule/${namespace}-*/*`],
+          }),
+          new iam.PolicyStatement({
+            actions: ["pipes:*"],
+            resources: [`arn:aws:pipes:*:*:pipe/${namespace}-*`],
+          }),
+          new iam.PolicyStatement({
+            actions: ["scheduler:*"],
+            resources: [`arn:aws:scheduler:*:*:schedule/${namespace}-*/*`],
+          }),
+          new iam.PolicyStatement({
+            actions: ["iam:PassRole"],
+            resources: [`arn:aws:iam:*:role/${namespace}-*`],
+            conditions: {
+              StringLike: { "iam:PassedToService": "scheduler.amazonaws.com" },
+            },
+          }),
+        ],
+      }),
+      [`${namespace}-sqs-policy`]: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["sqs:*"],
+            resources: [`arn:aws:sqs:*:*:${namespace}-*`],
+          }),
+        ],
+      }),
+      [`${namespace}-sfn-policy`]: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ["states:*"],
+            resources: [`arn:aws:states:*:*:stateMachine:${namespace}-*`],
+          }),
+        ],
+      }),
+    },
   }),
 })
