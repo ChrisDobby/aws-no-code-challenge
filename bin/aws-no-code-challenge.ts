3be2d94170a3342109e517ba1c169fa25fa21988
Chris Dobson <chrd@trustpilot.com>
2024-07-04 20:09:37 +0100

    pass region in context

--- bin/aws-no-code-challenge.ts
+++ bin/aws-no-code-challenge.ts
@@ -4,8 +4,12 @@ import * as cdk from "aws-cdk-lib"
 import { AwsNoCodeChallengeStack } from "../lib/aws-no-code-challenge-stack"
 
 const app = new cdk.App()
-new AwsNoCodeChallengeStack(app, "AwsNoCodeChallengeStack", {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const isBasic = app.node.tryGetContext("base") === "true"
+const region = app.node.tryGetContext("region") || process.env.CDK_DEFAULT_REGION
+const serviceName = app.node.tryGetContext("service") || "test"
+
+new AwsNoCodeChallengeStack(app, `AwsNoCodeChallengeStack-${serviceName}-${isBasic ? "basic" : ""}`, {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region },
   tags: {
     project: "aws-no-code-challenge",
   },
